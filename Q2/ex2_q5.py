58499299858cba9b150c953202a84c1d9fe4a89e
chaitaliarlekar <60788416+chaitaliarlekar@users.noreply.github.com>
2020-04-10 20:27:30 +0530

    Update ex2_q5.py

--- Q2/ex2_q5.py
+++ Q2/ex2_q5.py
@@ -1,30 +1,35 @@
 #Recommended to use python try-except block to perform error handling.
-from pprint import pprint 
+from pprint import pprint
 #use pprint instead of print to clearly print output documents
 from pymongo import MongoClient
 from pymongo.errors import ConnectionFailure,OperationFailure
-connectionString='mongodb+srv://201701101:201701101@mycluster-yony7.mongodb.net/test?retryWrites=true&w=majority'
-#enter your connection String here
+connectionString="mongodb+srv://201701101:201701101@nosql-mwo9a.mongodb.net/test?retryWrites=true&w=majority"
 client=MongoClient(connectionString)
+db=client["201701101"]
+mycol=db["Sales_replenish"]
 
 try:
     client.admin.command('ismaster')
 
 except ConnectionFailure:
     print('Server not available')
-    
+
 except OperationFailure:
     print('wrong credentials')
-    
+
 else:
     print('connected to database')
-    # write your code here
-    db = client['201701101']
-    pipeline=[]
-    pipeline.append({'$unwind':'$items'})
-    pipeline.append({'$group':{'_id':{'item_name':'$items.name','storeLocation':'$storeLocation'},'sales':{'$sum':'$items.quantity'}}})
-    pipeline.append({'$group':{'_id':'$_id.item_name','sales_history':{'$addToSet':{'storeLocation':'$_id.storeLocation','quantity':'$sales'}}}})
-    db.sales_replenish.insert_many(db.sales.aggregate(pipeline))
-    
+    val = db.Sales.aggregate([
+                {"$unwind" : {"path" : "$items"}},
+                {"$group" : {
+                        "_id" : "$items.name",
+                        "sales_history" : {"$push" : {"storeLocation" : "$storeLocation","quantity" : {"$multiply" : ["$items.price","$items.quantity"]}}}
+                            }
+                },
+                #{"$out" : "stock_replenish"}
+            ])
+    for v in val:
+        mycol.insert_one(v)
+
 finally:
 	client.close()
